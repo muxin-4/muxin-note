fc0cfe22ecac639b99720fbb724daa200b1a04df
charles <952046097@qq.com>
2018-08-06 23:34:52 +0800

    add

--- IT/front-end/JavaScript/design-patterns/design-patterns-project/src/index.js
+++ IT/front-end/JavaScript/design-patterns/design-patterns-project/src/index.js
@@ -1,27 +1,5 @@
-class Product {
-	constructor(name) {
-		this.name  = name
-	}
-	init() {
-		alert('init')
-	}
-	fun1() {
-		alert('fun1')
-	}
-	fun2() {
-		alert('fun2')
+class SingleObject {
+	login() {
+		
 	}
 }
-
-// 工厂
-class Creator {
-	create(name) {
-		return new Product(name)
-	}
-}
-
-// 测试
-let creator = new Creator()
-let p = creator.create('p1')
-p.init()
-p.fun1()
