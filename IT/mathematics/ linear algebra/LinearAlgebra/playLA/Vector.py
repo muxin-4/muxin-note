10e3fc0413a8242c043f9c16bc7b6e5bd2d00c25
charles <952046097@qq.com>
2019-01-29 21:37:09 +0800

    add

---
+++ IT/mathematics/ linear algebra/LinearAlgebra/playLA/Vector.py
@@ -0,0 +1,10 @@
+class Vector:
+
+    def __init__(self, lst):
+        self._values = lst
+
+    def __repr__(self):
+        return "Vector({})".format(self._values)
+
+    def __str__(self):
+        return "({})".format(", ".join(str(e) for e in self._values))
---
+++ IT/mathematics/ linear algebra/firstProject/main.py
@@ -0,0 +1,6 @@
+import numpy
+import scipy
+
+if __name__ == "__main__":
+    print('a' + numpy.__version__)
+    print(scipy.__version__)
